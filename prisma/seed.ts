5d8e40f0684d3e15d0dc5585502f060468be2c48
Roena0516 <tkf2001.yc@gmail.com>
2026-05-05 21:35:08 +0900

    fix :: 빌드 오류 수정

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../app/generated/prisma/client";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import "dotenv/config";
 
